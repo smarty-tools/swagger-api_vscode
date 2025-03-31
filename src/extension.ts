a5f6b4f6d713decc776996aeb6f5f8939f9fe33c
hejuxin <cuobingfeicuo@126.com>
2025-03-31 20:03:06 +0800

    feat: 添加命令

--- src/extension.ts
+++ src/extension.ts
@@ -20,6 +20,10 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+  context.subscriptions.push(vscode.commands.registerCommand('extension.getCurrentFilePath', async (uri) => {
+    vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '空'}`);
+  }));
 }
 
 // This method is called when your extension is deactivated
