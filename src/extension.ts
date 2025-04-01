1fc5d6fb219aeef589fe00d966018d28a86c08a1
hejuxin <cuobingfeicuo@126.com>
2025-04-01 11:53:24 +0800

    feat: 删除无用代码

--- src/extension.ts
+++ src/extension.ts
@@ -11,23 +11,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "swagger-api-tool" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('swagger-api-tool.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from swagger-api-tool!');
-	});
-
-	context.subscriptions.push(disposable);
 
   context.subscriptions.push(vscode.commands.registerCommand('extension.getCurrentFilePath', (uri) => {
     vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '空'}`);
     const contextPath = context.extensionUri.path;
     const filePath = `${contextPath}/src/package/index.js`;
 
-    exec(`node ${filePath} --o ${uri.path}`);
+    try {
+      exec(`node ${filePath} --o ${uri.path}`);
+    } catch(e) {
+      vscode.window.showInformationMessage(`e：${e}, ${JSON.stringify(e)}`);
+    }
+
 	}));
   
 }
